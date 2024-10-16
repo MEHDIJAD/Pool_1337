7450a055fab079e96aef77785828c8a94b7fab85
MEHDIJAD <elmehdielgarouaz@gmail.com>
2024-10-17 00:09:35 +0100

    done 17/10/2024

--- c04/conver_all.c
+++ c04/conver_all.c
@@ -18,25 +18,29 @@ int count_arr_size(int nb, char *base)
 
     while (nb > 0)
     {
-        nb /= ft_strlen(base);
+        nb /= ft_strlen(base); // nb to convert / base ==> lenght of string 
         count++;
     }
-    return (count);
+    return (count); // return size of array to creat 
 }
 
 
 void ft_putnbr_base(int nbr, char *base)
 {
-    int basen = ft_strlen(base);
-    int size = count_arr_size(nbr, base);
-    char arr[size];
+    int basen = ft_strlen(base); // base (2, 8, 10, 16 ...)
+    int size = count_arr_size(nbr, base); // size of array to fill 
+    char arr[size]; 
     int i;
 
     i = 0;
     while (nbr > 0)
     {
-        arr[i] = base[nbr % basen];
+        arr[i] = base[nbr % basen]; 
+        // exmple ==> arr[0] = base[42 % 16 = 10(index for A)]
+        // arr[1] = base[1] = base[2 % 16 = 2(index for 2)] 
         nbr /= basen;
+        // 42 / 16 = 2
+        // 2 / 16 = 0 (stop loop)
         i++;
     }
     i = size - 1;
@@ -49,5 +53,5 @@ void ft_putnbr_base(int nbr, char *base)
 
 int main(void)
 {
-    ft_putnbr_base(42, "dgefrf");
+    ft_putnbr_base(42, "0123456789ABCDEF");
 }
--- c04/converdic_binary.c
+++ c04/converdic_binary.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 
-int count(int nb)
+int count(int nb) // to count the size of array 
 {
     int count = 0;
 
     while (nb > 0)
     {
-        nb /= 2;
+        nb /= 2; 
         count++;
     }
+    //count = 2;
     return (count);
 }
 
 void  decimal_to_binary(int n)
 {
     int size = count(n);
-   int arr[size];
+   int arr[size]; // size = 2
    int i;
 
     i = 0;
-   while (n > 0)
+   while (n > 0) // converting end when n (the number we are converting is 0) = 0 
    {
-        arr[i] = n % 2;
+        arr[i] = n % 2; // to get conver
         n /= 2;
-        i++;
+        i++; // fill array 
    }
-   i = size - 1;
+   i = size - 1; // we start from last index (size - 1) because if size = 5, last index is 4
    while (i >= 0)
    {
     write(1, &arr[i], 1);
